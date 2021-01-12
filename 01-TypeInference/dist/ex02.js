a4abf8ebae017d7b843d3ea0e07abf2b172d0dac
Satish Kumar <satish.kumar06@nagarro.com>
2021-01-12 22:20:42 +0530

    Type Inference
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ 01-TypeInference/dist/ex02.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fruitName;
+fruitName = 'Apple';
+fruitName = 10;
+fruitName = ['hello'];
+console.log(fruitName);
+exports.default = null;
---
+++ 01-TypeInference/dist/ex03.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function print() {
+    console.log('hello world');
+}
+function sum(x, y) {
+    return x + y;
+}
+let output = print();
+console.log('output: ', output);
+console.log('sum: ', sum(10, 20));
+exports.default = null;
